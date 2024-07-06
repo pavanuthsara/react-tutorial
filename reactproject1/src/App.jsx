97a6a7406018ee00be08e2fdd684eef15569d31f
Pavan Uthsara <uthsarapavan@gmail.com>
2024-07-06 07:25:25 +0530

    tutorial 8

---
+++ reactproject1/src/App.jsx
@@ -0,0 +1,12 @@
+import Hello from "./components/Hello"
+
+function App() {
+  return (
+    <div className="App">
+      <Hello />
+      <Hello />
+    </div>
+  )
+}
+
+export default App
---
+++ reactproject1/src/components/Hello.jsx
@@ -0,0 +1,7 @@
+const name = "Pavan";
+
+function Hello(){
+    return <h1>Hello from, {name}</h1>
+}
+
+export default Hello;
