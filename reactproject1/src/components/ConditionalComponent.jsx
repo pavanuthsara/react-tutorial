f49068e1303f0984a5807f86509b069d5fd40b6a
Pavan Uthsara <uthsarapavan@gmail.com>
2024-07-09 21:30:28 +0530

    tutorial 17 : conditional rendering using element variables

--- reactproject1/src/components/ConditionalComponent.jsx
+++ reactproject1/src/components/ConditionalComponent.jsx
@@ -2,10 +2,13 @@ import Welcome from "./Welcome";
 import Code from "./Code";
 
 export default function ConditionalComponent() {
-  const display = true;
+  let message;
+  const display = false;
+
   if (display) {
-    return <Welcome />;
+    message = <h1>This is message 1</h1>;
   } else {
-    return <Code />;
+    message = <h1>This is message 2</h1>;
   }
+  return message;
 }
