3d363ccf41b088bc7f6234178d5b211f7b4400ce
Pavan Uthsara <uthsarapavan@gmail.com>
2024-07-10 22:13:52 +0530

    tutorial 18 : using ternary operators in react

--- reactproject1/src/components/ConditionalComponent.jsx
+++ reactproject1/src/components/ConditionalComponent.jsx
@@ -2,13 +2,7 @@ import Welcome from "./Welcome";
 import Code from "./Code";
 
 export default function ConditionalComponent() {
-  let message;
   const display = false;
 
-  if (display) {
-    message = <h1>This is message 1</h1>;
-  } else {
-    message = <h1>This is message 2</h1>;
-  }
-  return message;
+  return display ? <Welcome /> : <Code />;
 }
